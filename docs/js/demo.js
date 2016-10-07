2e48ab9d5fc7017300e797d80688ee5c08a12da3
LH <littleherry@gmail.com>
2016-10-07 18:51:32 +0800

    sdfsa

--- docs/js/demo.js
+++ docs/js/demo.js
@@ -1,8 +1,73 @@
-function init()
-{
-	document.write("<br / >");
-	document.write(document + "<br / >");
-	document.write("¡ô¡ô ¤W­±¬O document ªº¦r¦ê§Î¦¡<br / >");
-	document.write("<br / >");
-	document.write("µ{¦¡°õ¦æµ²§ô ...<br / >");	
+ï»¿var g_api_url = "https://slack.com/api/";
+var g_api_bot_token = "xoxb-88531198406-MvDzRsRO3KeID6gBLryXU4sY";
+var g_default_channel_id = "C2LFCAD7W";
+
+//$(document).ready(
+//	function() 
+//	{ 
+		// bind 'myForm' and provide a simple callback function 
+//); 
+
+function post(path, params, method)
+{	
+	method = method || "post";
+	
+	var form = document.createElement("form");
+	form.setAttribute("method", method);
+	form.setAttribute("action", path);
+	form.setAttribute("id", "Test");
+	form.setAttribute("accept-charset", "UTF-8");
+	
+	for (var key in params)
+	{
+		if (params.hasOwnProperty(key))
+		{
+			var hidden_field = document.createElement("input");
+			hidden_field.setAttribute("type", "hidden");
+			hidden_field.setAttribute("name", key);
+			hidden_field.setAttribute("value", params[key]);
+			
+			form.appendChild(hidden_field);
+		}
+	}
+	
+	document.body.appendChild(form);
+	form.submit();
 }
+
+function go_slack(func, params)
+{
+	post(g_api_url + func, params);
+}
+
+function talk(message)
+{
+	var params = 
+	{ 
+		token:g_api_bot_token, 
+		channel:g_default_channel_id, 
+		text:message, 
+		pretty:"1" 
+	};
+
+	go_slack("chat.postMessage", params);
+}
+
+function main()
+{	
+	//talk("æ¸¬è©¦æ¸¬è©¦æ¸¬è©¦è¬›è¬›è©±");	
+}
+        // wait for the DOM to be loaded 
+
+
+//$('#myForm').ajaxForm
+//(
+//	{
+//		//url: 'myscript.php', // or whatever
+//		dataType : 'json',
+//		success : function (response) 
+//		{
+//			alert("The server says: " + response);
+//		}
+//	}
+//);
